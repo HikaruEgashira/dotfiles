d258463f49f64aae65dacebca8ce1ede4d3ebcb9
HikaruEgashira <shighten@gmail.com>
2020-02-20 10:29:08 +0900

    fix

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 # nix
 curl https://nixos.org/nix/install | sh
-source ~/.nix-profile/etc/profile.d/nix.sh
+source $HOME/.nix-profile/etc/profile.d/nix.sh
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable
 nix-channel --update
 nix-env -iA nixpkgs.cachix
@@ -14,13 +14,14 @@ nix-env -iAnixpkgs.yarn
 curl -L https://git.io/n-install | bash
 
 # prezto
-zsh
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
-chsh -s /bin/zsh
+# zsh
+# git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+# setopt EXTENDED_GLOB
+# for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+#   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+# done
+# source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+# chsh -s /bin/zsh
 
-\cp -f .zshrc ~
+nix-collect-garbage
+\cp -f .zshrc $HOME
