8adfd140ffbf9d6524a77291cf2a0ef11e3c1974
Hikaru Egashira <shighten@gmail.com>
2022-01-08 12:55:36 +0900

    add chmod

--- install.sh
+++ install.sh
@@ -1,29 +1,25 @@
 # nix
-# mkdir /nix
-# chown alice /nix
-# curl https://nixos.org/nix/install | bash -s -- --no-daemon
-# source $HOME/.nix-profile/etc/profile.d/nix.sh
-# nix-channel --add https://nixos.org/channels/nixpkgs-unstable
-# nix-channel --update
-# nix-env -iA nixpkgs.cachix
-# nix-env -iA nixpkgs.zsh nixpkgs.ghq nixpkgs.peco
+echo "✔ install nix"
+mkdir /nix
+chown $USER /nix
+curl -L https://nixos.org/nix/install | sh
+source $HOME/.nix-profile/etc/profile.d/nix.sh
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+nix-channel --update
+
+# home-manager
+echo "✔ install home-manager"
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+nix-shell '<home-manager>' -A install
 
 # node
 # nix-env -iA nixpkgs.yarn
 # curl -L https://git.io/n-install | bash
-# nix-collect-garbage
 
-rm -rf ~/.zprezto ~/.zlogin ~/.zlogout ~/.zpreztorc ~/.zprofile ~/.zshenv ~/.zshrc
+# config
+cp -f .zshrc $HOME
 
-# prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
-
-\cp -f .zshrc $HOME
-\cp -f .zpreztorc $HOME
-
-echo "installed"
+nix-collect-garbage
+echo "✔ installed"
