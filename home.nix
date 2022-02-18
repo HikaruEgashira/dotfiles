51df4d6427c603c317b7b2195fb207740cea5216
hikae <contact@egahika.dev>
2022-02-18 20:53:34 +0900

    lorri

--- home.nix
+++ home.nix
@@ -2,16 +2,17 @@
 
 {
   programs.home-manager.enable = true;
+  services.lorri.enable = true;
 
   home.username = "egahika";
   home.homeDirectory = "/home/egahika";
   home.stateVersion = "21.11";
-  home.packages = with pkgs; [ lsd fd bat ghq fzf starship zsh nodejs direnv ];
+  home.packages = with pkgs; [ fd bat ghq fzf direnv ];
 
   programs.git = {
     enable = true;
     userName = "hikae";
-    userEmail = "account@egahika.dev";
+    userEmail = "contact@egahika.dev";
   };
   programs.lsd = {
     enable = true;
@@ -25,11 +26,10 @@
     enableCompletion = true;
     enableAutosuggestions = true;
     enableSyntaxHighlighting = true;
-    dotDir = ".config/zsh";
     shellAliases =
     {
       c = "code .";
-      rel = "source ~/.config/zsh/.zshrc";
+      rel = "source ~/.zshrc";
       ls = "lsd";
       fd = "fdfind";
       cat = "bat";
@@ -45,9 +45,10 @@ function fzf-src () {
 }
 zle -N fzf-src
 bindkey '^g' fzf-src
-source $HOME/.nix-profile/etc/profile.d/nix.sh
+# source $HOME/.nix-profile/etc/profile.d/nix.sh
 export PATH=$HOME/.docker/cli-plugins:$PATH
 export NODE_OPTIONS=--max_old_space_size=8192
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
 eval "$(direnv hook zsh)"
     '';
   };
--- readme.md
+++ readme.md
@@ -3,15 +3,7 @@
 https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH#how-to-install-zsh-on-many-platforms
 
 ```bash
-chsh -s $(which zsh)
+curl -L https://git.io/n-install | bash # WIP install node
 chmod 777 .
 sh install.sh
-```
-
-## other package
-
-### n
-
-```
-curl -L https://git.io/n-install | bash
 ```
