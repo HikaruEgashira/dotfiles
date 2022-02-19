be99416cfa67e8012c90d5d34b620d43bb273863
hikae <contact@egahika.dev>
2022-02-19 09:48:32 +0900

    refactor

--- install.sh
+++ install.sh
@@ -5,20 +5,20 @@ echo "✔ install nix"
 if [ ! -d /nix ]; then sudo mkdir /nix && sudo chown $USER /nix
 fi
 curl -L https://nixos.org/nix/install | sh
-source $HOME/.nix-profile/etc/profile.d/nix.sh
+. $HOME/.nix-profile/etc/profile.d/nix.sh
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable
-nix-channel --update
 
 # home-manager
 echo "✔ install home-manager"
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz home-manager
 nix-channel --update
-nix-env --set-flag priority 10 nix
-nix-env --set-flag priority 10 home-manager-path
+export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 nix-shell '<home-manager>' -A install
+. $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
 
 # sync
-ln -f ./home.nix ~/.config/nixpkgs
+ln -f ./home.nix ~/.config/nixpkgs # cp -f ./home.nix ~/.config/nixpkgs
+nix-env --set-flag priority 10 nix
 home-manager switch
 
 # end
--- readme.md
+++ readme.md
@@ -5,5 +5,12 @@ https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH#how-to-install-zsh-on-man
 ```bash
 curl -L https://git.io/n-install | bash # WIP install node
 chmod 777 .
-sh install.sh
+. ./install.sh
+```
+
+## enter zsh
+
+```bash
+. $HOME/.nix-profile/etc/profile.d/nix.sh
+zsh
 ```
