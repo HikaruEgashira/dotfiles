bdac15504e373ffb30e2578b6d803ad88fcfe62f
hikae <account@egahika.dev>
2023-12-29 10:57:13 +0000

    nerdctl setting

--- home.nix
+++ home.nix
@@ -51,6 +51,9 @@ function fzf-ghq () {
 }
 zle -N fzf-ghq
 bindkey '^g' fzf-ghq
+source <(nerdctl completion zsh)
+alias docker='nerdctl -n=k8s.io'
+complete -o default -F __start_nerdctl docker
     '';
   };
 }
