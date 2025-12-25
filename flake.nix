da3eba618d13de183d328f22576b6debfdbee28d
HikaruEgashira <account@egahika.dev>
2025-12-26 00:28:21 +0900

    feat: NixをFlakesベースに移行、GitHub Actionsをnixrunに対応
    - flake.nixを作成してFlakesベースのセットアップに対応
    - home-managerをnix runで実行可能に
    - CI/CDをnix flakeチェックと動作検証に更新
    - 実際のセットアップが検証される堅牢なパイプラインに改善

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Home manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+  let
+    username = "hikae";
+    system = "x86_64-linux";
+  in
+  {
+    homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      modules = [ ./home.nix ];
+    };
+  };
+}
