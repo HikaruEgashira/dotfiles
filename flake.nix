bee3cd126261e4c40a1e4d528e0bc4b3aee99aaf
HikaruEgashira <account@egahika.dev>
2025-12-26 00:29:25 +0900

    refactor: nix run home-manager として実行可能に

--- flake.nix
+++ flake.nix
@@ -13,11 +13,26 @@
   let
     username = "hikae";
     system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in
   {
     homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.${system};
+      inherit pkgs;
       modules = [ ./home.nix ];
+    };
+
+    apps.${system}.home-manager = {
+      type = "app";
+      program = "${pkgs.writeShellScriptBin "home-manager" ''
+        export FLAKE="''${FLAKE:-.}"
+        exec ${home-manager.packages.${system}.home-manager}/bin/home-manager \
+          --flake "$FLAKE#${username}" \
+          "$@"
+      ''}/bin/home-manager";
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [ home-manager nix ];
     };
   };
 }
