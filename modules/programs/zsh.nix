2f287532896d76c2a3eee5959a4e98e40caf3d0c
HikaruEgashira <account@egahika.dev>
2026-04-12 19:34:38 +0900

    feat: add tmux-new and tmux-kill aliases

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -27,7 +27,8 @@
       rel = "source ~/.zshrc";
       ghq = "gh q";
       say = "mise exec github:HikaruEgashira/say -- say";
-      tdev = "bash ~/dotfiles/modules/settings/tmux-dev.sh";
+      tmux-new = "bash ~/dotfiles/modules/settings/tmux-dev.sh";
+      tmux-kill = "tmux kill-session -t";
     };
 
     initContent = builtins.concatStringsSep "\n" [
