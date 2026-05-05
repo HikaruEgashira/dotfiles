a1f808f7ed87cc36db7093bf204b269686dbe5e5
HikaruEgashira <39324739+HikaruEgashira@users.noreply.github.com>
2026-05-05 22:59:55 +0900

    npmrc: enable pmsec hardening (min-release-age, audit-level, pnpm guards)

--- modules/home.nix
+++ modules/home.nix
@@ -12,6 +12,11 @@
         //registry.npmjs.org/:_authToken=''${NPM_TOKEN}
         registry=https://npm.flatt.tech/
         //npm.flatt.tech/:_authToken=''${FLATT_GUARD_TOKEN}
+        min-release-age=3
+        audit-level=high
+        minimum-release-age=4320
+        trust-policy=no-downgrade
+        strict-dep-builds=true
       '';
       force = true;
     };
